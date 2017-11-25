5c0f575baaf893bf251dd3bf33657324630fdc1b
lrf141 <ghost141.kentyo@gmail.com>
2017-11-25 14:16:49 +0900

    fix indent

--- fastecho_module.c
+++ fastecho_module.c
@@ -1,5 +1,5 @@
-#include<linux/module.h>
-#include<linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
 //module info
 MODULE_DESCRIPTION("The fastest echo server in kernel module");
@@ -8,22 +8,17 @@ MODULE_LICENSE("MIT");
 
 
 static int fastecho_init_module(void){
-  
-  printk("Fastest Echo Server!!");
-
-  return 0;
+        printk("Fastest Echo Server!!");
+        return 0;
 }
 
 
 static void fastecho_cleanup_module(void){
 
-  printk("Fastest Echo Server is unloaded!");
+        printk("Fastest Echo Server is unloaded!");
 
 }
 
 
-//add kernel load event(insmod)
 module_init(fastecho_init_module);
-
-//shutdown module event(rmmod)
 module_exit(fastecho_cleanup_module);
