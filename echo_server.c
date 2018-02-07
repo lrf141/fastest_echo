04ca886cab3a36b0c720167e45cb08a4d915d5cb
lrf141 <ghost141.kentyo@gmail.com>
2018-02-08 01:01:18 +0900

    リクエストを送り返す処理を追加

--- echo_server.c
+++ echo_server.c
@@ -20,6 +20,7 @@ static int get_request(struct socket *sock, char *buf, size_t size){
   msg.msg_namelen = 0;
   msg.msg_iov = &iov;
   msg.msg_iovlen = 1;
+  msg.msg_control = NULL;
   msg.msg_controllen = 0;
   msg.msg_flags = 0;
 
@@ -32,6 +33,46 @@ static int get_request(struct socket *sock, char *buf, size_t size){
   set_fs(oldfs);
 
   return length;
+}
+
+static int send_request(struct socket *sock, char *buf, size_t size){
+
+  mm_segment_t oldfs;
+  
+  struct iovec iov;
+  
+  struct msghdr msg;
+  msg.msg_name = 0;
+  msg.msg_namelen = 0;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  msg.msg_control = NULL;
+  msg.msg_controllen = 0;
+  msg.msg_flags = 0;
+
+  oldfs = get_fs();
+  set_fs(KERNEL_DS);
+  
+  int length;
+  int done = 0;
+
+  while(done < size){
+
+    iov.iov_base = (void *)((char *)buf + done);
+    iov.iov_len = size - done;
+    
+    length = sock_sendmsg(sock, &msg, iov.iov_len);
+    if(length < 0){
+      printk(KERN_ERR MODULE_NAME ": write error = %d\n", length);
+      break;
+    }
+
+    done += length;
+  }
+
+  set_fs(oldfs);
+
+  return done;
 }
 
 static int echo_server_worker(void *arg){
@@ -49,7 +90,25 @@ static int echo_server_worker(void *arg){
     return -1;
   }
 
+  int res;
+  while(!kthread_should_stop()){
+    res = get_request(sock, buf, BUF_SIZE);
+    if(res <= 0){
+      if(res){
+        printk(KERN_ERR MODULE_NAME ": get request error = %d\n", res);
+      }
+      break;
+    }
 
+    res = send_request(sock, buf, BUF_SIZE);
+    if(res < 0){
+      printk(KERN_ERR MODULE_NAME ": send request error = %d\n", res);
+      break;
+    }
+  }
+
+  kernel_sock_shutdown(socketm SHUT_RDWR);
+  sock_release(sock);
   kfree(buf);
   
   return 0;
